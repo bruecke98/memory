54ebf8e5ebb49b24c99aa14ac37a55a1a69d4772
bruecke98 <brueckmann.paul@gmail.com>
2022-05-15 10:06:58 +0200

    error

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,12 @@
 
 int main(int argc, char** argv) {
     // TODO Kreativaufgabe 
-    cppp::Map map(16);
-    std::vector<cppp::Item> a = { { 1, "a" }, { 1, "a" } };
+    //cppp::Map map(16);
+    std::vector<cppp::Item> a = { { 1, "a" }, { 2, "b" } };
     std::vector<cppp::Item> b = { { 3, "c" }, { 4, "d" } };
-    
+
+    cppp::Map map(16);
+
     map.insert("a", a);
     map.insert("a", a);
     map.insert("b", b);
--- src/map.cpp
+++ src/map.cpp
@@ -7,60 +7,124 @@
 
 cppp::Map::Map(const std::size_t size) : size(size) {
     // TODO
-    //const s = size;
-    //arr[15];
-    MapBucket *arr = new MapBucket(size);
-    
-    
-    
-    //std::cout << arr1 << std::endl;
+    arr = new MapBucket(size);
+    /**
+    MapBucket mB(size);
+    MapElement mapE;
+    Item it;
+    it.anzahl = 1;
+    it.bezeichnung = "";
+    mapE.key = "";
+    mapE.value = {it};
 
-    //std::vector<MapElement> arr; 
+    mB.at(0) = mapE;
+    for (size_t i = 0; i < size-1; i++)
+    {
+        std::cout << i << std::endl;
+        arr[i] = {mB};
+    }
 
-
+    **/
+    
+        
+    
+    //arr[0] ={mB};
+    std::cout << arr->size() << std::endl; 
 }
 
 cppp::Map::~Map() {
     // TODO
     delete[] arr;
+    arr = nullptr;
 }
 
 void cppp::Map::insert(const std::string& key, const std::vector<Item>& order) {
     // TODO
-    MapBucket arr;
-    //arr.resize(size);
-    //arr.reserve(size);
-    std::size_t hash = calcHash(key);
-    
-    
-    //speichert nicht korrekt in arr
-    std::cout << arr.at(0).key.data() << std::endl;
-    
-    if (!arr.empty()){ 
-        
+    //calculate hash
+     std::size_t hash = calcHash(key);
+    //check if *arr at position hash is empty
+    if (&arr[hash].empty){
+        //create a MapElemnt 
+        MapElement mapEl;
+        mapEl.key = key;
+        mapEl.value = order;
+        //create MapBucket
+        MapBucket mapBu(size);
+        mapBu.push_back(mapEl);
 
+        //write the MapBucket in arr at position hash
+        //ERROR program stops working
+        arr[hash] = mapBu;
+        //&arr[hash].push_back(mapEl);
+
+    }else{
+
+    }
+
+    // insert a MapElement in *arr (MapBucket)
+    // MapElement is 
+    //MapBucket arr;
+
+
+
+/**
+
+        std::size_t hash = calcHash(key);
+
+
+               std::cout << &arr[hash].empty << std::endl;
+               std::cout << &arr[hash].at(0).key << std::endl;
+
+
+
+
+       // arr = this->arr;
+    //MapBucket *arr = new MapBucket[size];
+    MapBucket mB(size);
+    MapElement mapE;
+    Item it;
+    it.anzahl = 1;
+    it.bezeichnung = "r";
+    mapE.key = "a";
+    mapE.value = {it};
+
+    mB.push_back(mapE); **/
+    //arr[hash] ={mB};
+   // mB.at(0) = {mapE};
+    //arr->at(0) = 
+        //std::cout << arr[hash].at(0).key << std::endl;
+        //std::cout << &arr << std::endl;
+        //std::cout << arr +1  << std::endl;
+        //std::cout << arr[hash] << std::endl;
+
+
+
+    /**
+    if (true){
+
+        MapBucket *arr = new MapBucket[size];
+        MapBucket mB(size);
         MapElement mapE;
         mapE.key = key;
         mapE.value = order;
-        //arr.resize(size);
-        arr.at(0) = mapE;
-        //arr.at(hash) = mapE;
-        //arr.assign(*mapE);
 
+        mB.at(0) = mapE;
+        arr[hash] ={mB};
+                                    std::cout << "erster" << std::endl;
 
-        
-        std::cout  << arr.at(0).value.at(0).anzahl <<  " - made it" << std::endl;
-
-
-
-        
     }else{
-
-        
-
-        std::cout << "zweiter" << std::endl;
+                            std::cout << "zweiter" << std::endl;
 
     }
+
+    **/
+
+
+ 
+
+                std::cout << "geschafft" << std::endl;
+
+
 }
 
 std::vector<cppp::Item> cppp::Map::get(const std::string& key) {
@@ -72,5 +136,11 @@ void cppp::Map::remove(const std::string& key) {
 }
 
 std::size_t cppp::Map::calcHash(const std::string& key) {
-    return key.length() % size; // TODO
+    size_t sum = 0;
+    for (char i : key) {
+        sum += i;
+    }
+    //std::cout << sum << std::endl;
+
+    return sum % size; // TODO
 }
